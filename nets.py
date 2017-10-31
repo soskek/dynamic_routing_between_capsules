be13fe8eb91954fcf7f391a1cdfcf9616449dcef
Sosuke Kobayashi <sosk@preferred.jp>
2017-11-01 01:50:08 +0900

    fix bugs

--- nets.py
+++ nets.py
@@ -1,10 +1,9 @@
 import numpy as np
+
 import chainer
 from chainer import cuda
 import chainer.functions as F
 import chainer.links as L
-from chainer import training
-from chainer.training import extensions
 
 
 def _augmentation(x):
@@ -20,90 +19,93 @@ def _augmentation(x):
     return a
 
 
+def _count_params(m):
+    print('# of params', sum(param.size for param in m.params()))
+
+
+init = chainer.initializers.Uniform(scale=0.05)
+
+
 class CapsNet(chainer.Chain):
 
     def __init__(self):
         super(CapsNet, self).__init__()
         with self.init_scope():
-            self.conv1 = L.Convolution2D(1, 256, ksize=9, stride=1)
-            self.conv2 = L.Convolution2D(256, 256, ksize=9, stride=2)
+            self.conv1 = L.Convolution2D(1, 256, ksize=9, stride=1,
+                                         initialW=init)
+            self.conv2 = L.Convolution2D(256, 32 * 8, ksize=9, stride=2,
+                                         initialW=init)
             self.Ws = chainer.ChainList(
-                *[L.Convolution2D(8, 16 * 10, ksize=1, stride=1)
+                *[L.Convolution2D(8, 16 * 10, ksize=1, stride=1, initialW=init)
                   for i in range(32)])
 
-            # TODO: use NDconv
-        #"""
-        for name, param in self.namedparams():
-            if param.ndim != 1:
-                # This initialization is applied only for weight matrices
-                param.data[...] = np.random.uniform(
-                    -0.01, 0.01, param.data.shape)
-        #"""
+            self.fc1 = L.Linear(16 * 10, 512, initialW=init)
+            self.fc2 = L.Linear(512, 1024, initialW=init)
+            self.fc3 = L.Linear(1024, 784, initialW=init)
+
+        _count_params(self)
+        self.results = {'N': 0, 'loss': [], 'correct': []}
+
+    def pop_results(self):
+        mean_loss = sum(self.results['loss']) / self.results['N']
+        accuracy = sum(self.results['correct']) / self.results['N']
+        self.results = {'N': 0, 'loss': [], 'correct': []}
+        return mean_loss, accuracy
 
     def __call__(self, x, t):
-        #x = _augmentation(x)
+        if chainer.config.train:
+            x = _augmentation(x)
         out, _ = self.output(x)
         self.loss = self.calculate_loss(out, t)
-        self.accuracy = self.calculate_accuracy(out, t)
-        print(self.loss, self.accuracy)
-        return self.loss
 
-    def evaluate(self, x, t):
-        out, _ = self.output(x)
-        self.loss = self.calculate_loss(out, t)
-        self.accuracy = self.calculate_accuracy(out, t)
-        return self.loss.data, self.accuracy
+        self.results['loss'].append(self.loss.data)
+        self.results['correct'].append(self.calculate_correct(out, t))
+        return self.loss
 
     def output(self, x):
         batchsize = x.shape[0]
         n_iterations = 3
 
         h1 = F.relu(self.conv1(x))
-        primary_caps_concat = self.conv2(h1)  # relu required?
-        primary_caps = F.split_axis(primary_caps_concat, 32, axis=1)
+        pr_caps = F.split_axis(self.conv2(h1), 32, axis=1)
 
-        # TODO: do not average it
         Preds = []
         for i in range(32):
-            pred = self.Ws[i](primary_caps[i])
-            assert(pred.shape[1] == 160)
-            assert(pred.shape[2] == 6)
-            assert(pred.shape[3] == 6)
-            Pred = pred.reshape((batchsize, 10, 16, 6 * 6))
-            #Pred = pred.reshape((batchsize, 160, 6 * 6))
-            #Pred = Pred.reshape((batchsize, 10, 16, 6 * 6))
+            pred = self.Ws[i](pr_caps[i])
+            Pred = pred.reshape((batchsize, 16, 10, 6 * 6))
             Preds.append(Pred)
+        Preds = F.stack(Preds, axis=3)
+        assert(Preds.shape == (batchsize, 16, 10, 32, 6 * 6))
 
-        bs = [self.xp.zeros((batchsize, 10, 1, 6 * 6), dtype='f')] * 32
+        bs = self.xp.zeros((batchsize, 10, 32, 6 * 6), dtype='f')
         for i_iter in range(n_iterations):
-            ss = self.xp.zeros((batchsize, 10, 16), dtype='f')
-            for i in range(32):
-                b = bs[i]
-                c = F.softmax(b, axis=1)
-                C = F.broadcast_to(c, (batchsize, 10, 16, 6 * 6))
-                ss = ss + F.sum(C * Preds[i], axis=3)
-            ss_norm = F.sum(ss ** 2, axis=2)
-            Ss_norm = F.broadcast_to(ss_norm[:, :, None], ss.shape)
-            vs = Ss_norm / (1. + Ss_norm) * ss / (Ss_norm ** 0.5)
-            # (batchsize, 10, 16)
+            cs = F.softmax(bs, axis=1)
+            Cs = F.broadcast_to(cs[:, None], (batchsize, 16, 10, 32, 6 * 6))
+            ss = F.sum(Cs * Preds, axis=(3, 4))
+            ss_norm = F.sum(ss ** 2, axis=1, keepdims=True)
+            ss_norm = F.broadcast_to(ss_norm, ss.shape)
+            vs = ss_norm / (1. + ss_norm) * ss / (ss_norm ** 0.5)
+            # (batchsize, 16, 10)
 
             if i_iter != n_iterations - 1:
-                for i in range(32):
-                    Vs = F.broadcast_to(vs[:, :, :, None], Preds[i].shape)
-                    bs[i] = bs[i] + F.sum(Vs * Preds[i], axis=2, keepdims=True)
-                    # F.sum(vs * Preds[i], axis=2)
+                Vs = F.broadcast_to(vs[:, :, :, None, None], Preds.shape)
+                # (batchsize, 16, 10, 32, 6 * 6)
+                bs = bs + F.sum(Vs * Preds, axis=1)
 
-        vs_norm = F.sum(vs ** 2, axis=2) ** 0.5
+        vs_norm = F.sum(vs ** 2, axis=1) ** 0.5
         return vs_norm, vs
 
     def calculate_loss(self, v, t):
-        T = self.xp.zeros(v.shape, dtype='f')
-        T[t] = 1.
-        m = self.xp.full(v.shape, 0.1, dtype='f')
-        m[t] = 0.9
-        loss = T * F.relu(m - v) ** 2 + 0.5 * (1. - T) * F.relu(v - m) ** 2
-        # return F.sum(loss)
-        return F.mean(loss) * 10
+        batchsize = t.shape[0]
+        xp = self.xp
+        I = xp.arange(batchsize)
 
-    def calculate_accuracy(self, v, t):
-        return (self.xp.argmax(v.data, axis=1) == t).mean()
+        T = xp.zeros(v.shape, dtype='f')
+        T[I, t] = 1.
+        m = xp.full(v.shape, 0.1, dtype='f')
+        m[I, t] = 0.9
+        loss = T * F.relu(m - v) ** 2 + 0.5 * (1. - T) * F.relu(v - m) ** 2
+        return F.sum(loss) / batchsize
+
+    def calculate_correct(self, v, t):
+        return (self.xp.argmax(v.data, axis=1) == t).sum()
--- train.py
+++ train.py
@@ -1,44 +1,28 @@
-#!/usr/bin/env python
-"""Fully-connected neural network example using MNIST dataset
-
-This code is a custom loop version of train_mnist.py. That is, we train
-models without using the Trainer class in chainer and instead write a
-training loop that manually computes the loss of minibatches and
-applies an optimizer to update the model.
-"""
 from __future__ import print_function
 
 import argparse
+import json
 
 import chainer
-from chainer.dataset import convert
-import chainer.links as L
-from chainer import serializers
+from chainer.dataset.convert import concat_examples
 
 import nets
+
+DECAY_LR = 0.8
 
 
 def main():
     parser = argparse.ArgumentParser(description='Chainer example: MNIST')
-    parser.add_argument('--batchsize', '-b', type=int, default=100,
+    parser.add_argument('--batchsize', '-b', type=int, default=128,
                         help='Number of images in each mini-batch')
-    parser.add_argument('--epoch', '-e', type=int, default=20,
+    parser.add_argument('--epoch', '-e', type=int, default=30,
                         help='Number of sweeps over the dataset to train')
     parser.add_argument('--gpu', '-g', type=int, default=-1,
                         help='GPU ID (negative value indicates CPU)')
     parser.add_argument('--out', '-o', default='result',
                         help='Directory to output the result')
-    parser.add_argument('--resume', '-r', default='',
-                        help='Resume the training from snapshot')
-    parser.add_argument('--unit', '-u', type=int, default=1000,
-                        help='Number of units')
     args = parser.parse_args()
-
-    print('GPU: {}'.format(args.gpu))
-    print('# unit: {}'.format(args.unit))
-    print('# Minibatch-size: {}'.format(args.batchsize))
-    print('# epoch: {}'.format(args.epoch))
-    print('')
+    print(json.dumps(args.__dict__, indent=2))
 
     # Set up a neural network to train
     model = nets.CapsNet()
@@ -48,47 +32,38 @@ def main():
         model.to_gpu()  # Copy the model to the GPU
 
     # Setup an optimizer
-    optimizer = chainer.optimizers.Adam(alpha=1e-4)  # 1e-3 def
+    optimizer = chainer.optimizers.Adam(alpha=1e-3)
     optimizer.setup(model)
 
     # Load the MNIST dataset
     train, test = chainer.datasets.get_mnist(ndim=3)
     train_iter = chainer.iterators.SerialIterator(train, args.batchsize)
-    test_iter = chainer.iterators.SerialIterator(test, args.batchsize,
+    test_iter = chainer.iterators.SerialIterator(test, 100,
                                                  repeat=False, shuffle=False)
 
-    sum_accuracy = 0
-    sum_loss = 0
-
+    print('TRAINING starts')
     while train_iter.epoch < args.epoch:
         batch = train_iter.next()
-        x, t = convert.concat_examples(batch, args.gpu)
+        x, t = concat_examples(batch, args.gpu)
         optimizer.update(model, x, t)
-        sum_loss += float(model.loss.data) * len(t)
-        sum_accuracy += float(model.accuracy) * len(t)
 
         # evaluation
         if train_iter.is_new_epoch:
-            print('epoch: ', train_iter.epoch)
-            print('train mean loss: {}, accuracy: {}'.format(
-                sum_loss / len(train), sum_accuracy / len(train)))
-            sum_accuracy = 0
-            sum_loss = 0
+            mean_loss, accuracy = model.pop_results()
+            print('epoch {}\ttrain mean loss: {}, accuracy: {}'.format(
+                train_iter.epoch, mean_loss, accuracy))
+
             for batch in test_iter:
-                x, t = convert.concat_examples(batch, args.gpu)
-                loss, accuracy = model.evaluate(x, t)
-                sum_loss += float(loss) * len(t)
-                sum_accuracy += float(accuracy) * len(t)
+                x, t = concat_examples(batch, args.gpu)
+                with chainer.no_backprop_mode():
+                    with chainer.using_config('train', False):
+                        loss = model(x, t)
 
             test_iter.reset()
-            print('test mean  loss: {}, accuracy: {}'.format(
-                sum_loss / len(test), sum_accuracy / len(test)))
-            sum_accuracy = 0
-            sum_loss = 0
-
-    # Save the model and the optimizer
-    print('save the model')
-    serializers.save_npz('mlp.model', model)
+            mean_loss, accuracy = model.pop_results()
+            print('\t\ttest mean  loss: {}, accuracy: {}'.format(
+                mean_loss, accuracy))
+            optimizer.alpha *= DECAY_LR
 
 
 if __name__ == '__main__':

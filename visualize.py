0e5029c7bfe017558ba6dee2ed39398296ce2ff0
Sosuke Kobayashi <sosk@preferred.jp>
2017-11-05 19:07:55 +0900

    add other visualization

--- visualize.py
+++ visualize.py
@@ -19,17 +19,20 @@ def save_images(xs, filename):
     height = len(xs)
 
     xs = [np.array(x.tolist(), np.float32) for x in xs]
-    fig, ax = plt.subplots(height, width, figsize=(1 * width, height))
+    # subplots with many figs are very slow
+    fig, ax = plt.subplots(
+        height, width, figsize=(1 * width / 2.5, height / 2.5))
     xs = np.concatenate(xs, axis=0)
-
-    for ai, xi in zip(ax.ravel(), xs):
+    for i, (ai, xi) in enumerate(zip(ax.ravel(), xs)):
         ai.set_xticklabels([])
         ai.set_yticklabels([])
         ai.set_axis_off()
-        ai.imshow(xi.reshape(28, 28), cmap='Blues_r', vmin=0., vmax=1.)
+        color = 'Greens_r' if i < width else 'Blues_r'  # first line
+        ai.imshow(xi.reshape(28, 28), cmap=color, vmin=0., vmax=1.)
 
     plt.subplots_adjust(
-        left=None, bottom=None, right=None, top=None, wspace=0.1, hspace=0.)
+        left=None, bottom=None, right=None, top=None, wspace=0., hspace=0.)
+    # saving and clearing subplots with many figs are also very slow
     fig.savefig(filename, bbox_inches='tight', pad=0.)
     plt.clf()
     plt.close('all')
@@ -42,17 +45,43 @@ def visualize_reconstruction(model, x, t, filename='vis.png'):
                 filename)
 
 
-def visualize_reconstruction_all(model, x, t, filename='visall.png'):
-    t_list = []
+def visualize_reconstruction_alldigits(model, x, t, filename='vis_all.png'):
     x_recon_list = []
     vs_norm, vs = model.output(x)
     for i in range(10):
         pseudo_t = model.xp.full(t.shape, i).astype('i')
         x_recon = model.reconstruct(vs, pseudo_t).data
-        t_list.append(pseudo_t)
         x_recon_list.append(x_recon)
     save_images([x] + x_recon_list,
                 filename)
+
+
+def visualize_reconstruction_tweaked(model, x, t, filename='vis_tweaked.png',
+                                     dim_idx=0):
+    assert(0 <= dim_idx <= 15)
+    x_recon_list = []
+    vs_norm, vs = model.output(x)
+    vs = vs.data
+    for i in range(9):
+        tweaked_vs = model.xp.array(vs)
+        tweaked_vs[:, dim_idx, :] = (i - 4.) * 0.05  # [-0.20, 0.20]
+        x_recon = model.reconstruct(tweaked_vs, t).data
+        x_recon_list.append(x_recon)
+    x_recon = model.reconstruct(vs, t).data
+    save_images([x_recon] + x_recon_list,
+                filename)
+
+
+def get_samples(dataset):
+    # 2 samples for each digit
+    samples = []
+    for i, (x, t) in enumerate(dataset):
+        if t == len(samples) // 2:
+            print('{}-th sample is used'.format(i))
+            samples.append((x, t))
+        if len(samples) >= 20:
+            break
+    return samples
 
 
 if __name__ == '__main__':
@@ -68,10 +97,17 @@ if __name__ == '__main__':
     if args.gpu >= 0:
         chainer.cuda.get_device_from_id(args.gpu).use()
         model.to_gpu()
-    train, test = chainer.datasets.get_mnist(ndim=3)
+    _, test = chainer.datasets.get_mnist(ndim=3)
 
-    batch = test[50:60]
+    batch = get_samples(test)
     x, t = concat_examples(batch, args.gpu)
 
-    visualize_reconstruction(model, x, t)
-    visualize_reconstruction_all(model, x, t)
+    with chainer.no_backprop_mode():
+        with chainer.using_config('train', False):
+            visualize_reconstruction(model, x, t)
+            visualize_reconstruction_alldigits(model, x, t)
+            for i in range(16):
+                visualize_reconstruction_tweaked(
+                    model, x, t,
+                    filename='vis_tweaked{}.png'.format(i),
+                    dim_idx=i)
